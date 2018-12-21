b3a790deea760a1c9db33030a8eac17c40eb1843
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2018-12-21 12:01:36 +0100

    Added first implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+# perfana-hello-world-events
+
+Example implementation of the PerfanaTestEvent interface.
+
+Shows when events are called in System.out.
+
+Also prints out the env variables and the PerfanaEventProperties in 
+the start test event call.
---
+++ src/main/java/nl/stokpop/perfana/event/StokpopHelloPerfanaEvent.java
@@ -0,0 +1,44 @@
+package nl.stokpop.perfana.event;
+
+import io.perfana.service.PerfanaTestEventAdapter;
+
+import java.util.Map;
+
+public class StokpopHelloPerfanaEvent extends PerfanaTestEventAdapter {
+
+    private final static String PERFANA_EVENT_NAME = StokpopHelloPerfanaEvent.class.getCanonicalName();
+
+    {
+        say("Class loaded");
+        System.getenv().forEach((key, value) -> say(String.format("env: %s=%s", key, value)));
+    }
+
+    public StokpopHelloPerfanaEvent() {
+        say("Default constructor called.");
+    }
+
+    @Override
+    public void beforeTest(String testId, Map<String,String> properties) {
+        say("Hello before test  [" + testId + "]");
+        say("Perfana event properties: " + properties);
+    }
+
+    @Override
+    public void afterTest(String testId, Map<String,String> properties) {
+        say("Hello after test [" + testId + "]");
+    }
+
+    @Override
+    public void failover(String testId, Map<String,String> properties) {
+        say("Hello failover " + testId + "]");
+    }
+
+    @Override
+    public void keepAlive(final String testId, final Map<String, String> eventProperties) {
+        say("Hello keep alive for test [" + testId + "]");
+    }
+
+    public static void say(String something) {
+        System.out.printf("[%s] %s%n", PERFANA_EVENT_NAME, something);
+    }
+}
